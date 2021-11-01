233ef6fbba5e810de74a12fd52b6cbcdf671cc76
petrn777 <novak.pe.2019@skola.ssps.cz>
2021-11-01 17:04:22 +0100

    State komponenta ContentItem

--- 01-11-21/src/App.js
+++ 01-11-21/src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
+import ContentItem from './ContentItem';
 
 function App() {
+
+  const [count, setCount] = useState(0);
+
+
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Text</p>
+        <p>Hodnota počtu: {count}</p>
+
+        <div>
+          <button onClick={()=> {
+          
+          console.log("User click");
+          setCount(count+1);          
+          
+          }}>Pričti</button>
+        </div>
+
+        <div>
+          <ContentItem text = "Hoj"/>
+        </div>
       </header>
     </div>
   );
 }
+
 
 export default App;
---
+++ 01-11-21/src/ContentItem.js
@@ -0,0 +1,16 @@
+const ContentItem = ({text}) =>
+{
+
+
+
+    return(
+        <div>
+            <p>Content item</p>
+            <p>{text}</p>
+        </div>
+
+    );
+}
+
+export default ContentItem;
+
